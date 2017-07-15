3d8a0beb8d75e9a798cedb1afa3d0e7b9a075718
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-07-15 23:58:39 +0200

    regrouping scss files

--- src/components/saveInfo.js
+++ src/components/saveInfo.js
@@ -19,31 +19,31 @@ export class SaveInfo extends Component {
   render() {
       if(this.props.saveInfo){
         return (
-          <div className="saveRemoveCNT">
-            <p className="saveRemove-txt">Trening został właśnie zapisany na Twoje konto</p>
-            <button className="save-btn" onClick={this.closeSaveInfoAndIsblock}>OK</button>
+          <div className="fullScreenInfo-CNT">
+            <p className="fullScreenInfo-txt">Trening został właśnie zapisany na Twoje konto</p>
+            <button className="green-btn" onClick={this.closeSaveInfoAndIsblock}>OK</button>
           </div>
         )
       } else if(this.props.isBlocking){
         return (
-          <div className="saveRemoveCNT">
-            <p className="saveRemove-txt">Trening nie mógł być zapisany ponieważ te konto już istnieje lub ma przypisany trening</p>
-            <button className="save-btn" onClick={this.closeSaveInfoAndIsblock}>OK</button>
+          <div className="fullScreenInfo-CNT">
+            <p className="fullScreenInfo-txt">Trening nie mógł być zapisany ponieważ te konto już istnieje lub ma przypisany trening</p>
+            <button className="green-btn" onClick={this.closeSaveInfoAndIsblock}>OK</button>
           </div>
         )
       } else if(this.props.isBlockingRemove){
         return (
-          <div className="saveRemoveCNT">
-            <p className="saveRemove-txt">Czy na pewno chcesz usunąć trening?</p>
-            <button className="save-btn" onClick={this.removeAccountCondition}>Tak</button>
-            <button className="remove-btn" onClick={this.closeSaveInfoAndIsblock}>Nie</button>
+          <div className="fullScreenInfo-CNT">
+            <p className="fullScreenInfo-txt">Czy na pewno chcesz usunąć trening?</p>
+            <button className="green-btn" onClick={this.removeAccountCondition}>Tak</button>
+            <button className="red-btn" onClick={this.closeSaveInfoAndIsblock}>Nie</button>
           </div>
         )
       }  else if(this.props.removeInfo){
         return (
-          <div className="saveRemoveCNT">
-            <p className="saveRemove-txt">Trening został usuniety.</p>
-            <button className="save-btn" onClick={this.closeSaveInfoAndIsblock}>OK</button>
+          <div className="fullScreenInfo-CNT">
+            <p className="fullScreenInfo-txt">Trening został usuniety.</p>
+            <button className="green-btn" onClick={this.closeSaveInfoAndIsblock}>OK</button>
           </div>
         )
       } else {
